b747654f0afbc40ba6700cb3a1eae8ab1fdf2d79
Lucas Loureiro <lucas-loureiro@outlook.com>
2021-12-06 10:06:50 -0300

    Updates

---
+++ precache-manifest.8bd61ff7a9e1dcadf886992a4b87afe3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "66febd52da23e43ea066a47a0d2b3b69",
+    "url": "/project-trivia-react-redux/index.html"
+  },
+  {
+    "revision": "1bda18e568b6b19315f8",
+    "url": "/project-trivia-react-redux/static/css/main.0b75654b.chunk.css"
+  },
+  {
+    "revision": "1f92ec1643034c5a3cac",
+    "url": "/project-trivia-react-redux/static/js/2.18ad7c0f.chunk.js"
+  },
+  {
+    "revision": "b51e62edbcf7a9c4f4ad13f506021d3e",
+    "url": "/project-trivia-react-redux/static/js/2.18ad7c0f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1bda18e568b6b19315f8",
+    "url": "/project-trivia-react-redux/static/js/main.a11215bf.chunk.js"
+  },
+  {
+    "revision": "49fe4f078093e17d3305",
+    "url": "/project-trivia-react-redux/static/js/runtime-main.e747d5a1.js"
+  },
+  {
+    "revision": "466d153e266e6e813c5ef2c55aa5fc2c",
+    "url": "/project-trivia-react-redux/static/media/trivia.466d153e.png"
+  }
+]);
